42d4b81b2b43e319a434932a4cb625f1eacd67f6
ParkDoheum <pirbak@gmail.com>
2022-11-04 18:03:45 +0900

    22-11-04-마무으리

---
+++ js/my3/js/joinForm.js
@@ -0,0 +1,63 @@
+const $frm = document.querySelector('#frm');
+$frm.addEventListener('keydown', e => {
+    if(e.key === 'Enter') {
+        e.preventDefault();
+    }
+});
+
+function emptyValChk(ele, name) {
+    if(!ele.value) {
+        alert(`${name}을(를) 입력해주세요.`);
+        return true;
+    }
+    return false;
+}
+
+function chk() {
+    if( emptyValChk($frm.uid, '아이디')  
+        ||  emptyValChk($frm.upw, '비밀번호') 
+        ||  emptyValChk($frm.email, '이메일') 
+        ||  emptyValChk($frm.state_msg, '상태메시지') 
+    ) {
+        return false;
+    }
+    if($frm.upw.value !== $frm.re_upw.value) {
+        alert('비밀번호를 확인해주세요.');
+        return false;
+    }
+
+    const pwReg = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}$/;
+    if(!pwReg.test($frm.upw.value)) {
+        alert('비밀번호는 문자,숫자,특수문자 포함한 8자리 이상이어야 합니다.');
+        return false;
+    }
+    return true;
+}
+
+/*
+function chk() {        
+    if(!$frm.uid.value) {
+        alert('uid를 입력해주세요.');
+        return false;
+    }
+    if(!$frm.upw.value) {
+        alert('upw를 입력해주세요.');
+        return false;
+    }
+    if($frm.upw.value !== $frm.re_upw.value) {
+        alert('비밀번호를 확인해주세요.');
+        return false;
+    }
+    if(!$frm.email.value) {
+        alert('이메일을 입력해주세요.');
+        return false;
+    }
+
+    //비밀번호, 확인비밀번호 값이 같은지 확인
+    //다르면
+    // alert "비밀번호를 확인해주세요." 띄우고
+    // return false;
+
+    return true;    
+}
+*/
