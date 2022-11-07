8a22a889230e5b5238b3d4cd31d5a9ee4683132c
ParkDoheum <pirbak@gmail.com>
2022-11-07 17:00:32 +0900

    22-11-07-영화 제목 리스트 출력

--- js/movie/js/searchDailyBoxOfficeList.js
+++ js/movie/js/searchDailyBoxOfficeList.js
@@ -6,6 +6,7 @@
     const frmElem = document.querySelector('#searchFrm');
     const dateElem = frmElem.targetDt;
     const searchBtnElem = frmElem.search;
+    const contentsElem = document.querySelector('#contents');
 
     window.addEventListener('load', e => {
         const now = new Date();
@@ -18,12 +19,30 @@
     }
     */
     searchBtnElem.addEventListener('click', e => {
+        contentsElem.innerHTML = null;
+
         const val = dateElem.value.replaceAll('-', '');
         console.log(val);
 
         const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${KEY}&targetDt=${val}`;
         console.log(url);
+
+        fetch(url)
+        .then(res => res.json())
+        .then(makeList);
     });
 
+    function makeList(data) {        
+        data.boxOfficeResult.dailyBoxOfficeList.forEach(makeRow);
+    }
+
+    function makeRow(item) {
+        console.log(item);
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <h3>${item.rank}. ${item.movieNm}</h3>
+        `;
+        contentsElem.append(div);
+    }
 
 })();
