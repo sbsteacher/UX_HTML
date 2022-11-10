63edbc0f434b4b617ebef6bfbf93545afb8bf824
ParkDoheum <pirbak@gmail.com>
2022-11-10 17:48:43 +0900

    22-11-10-detail.js 작업중(4)

--- js/goodFood/js/detail.js
+++ js/goodFood/js/detail.js
@@ -2,20 +2,28 @@
     'use strict';
     var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-    };
+    
 
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    var map = null;
 
     const $contents = document.querySelector('#contents');
 
     const savedJson = localStorage.getItem('item');
     const itemObj = JSON.parse(savedJson);
 
-    var geocoder = new kakao.maps.services.Geocoder();
-    console.log(geocoder);
+    var geocoder = new kakao.maps.services.Geocoder();    
+    geocoder.addressSearch(itemObj.GNG_CS, (result, status) => {
+        if(status === kakao.maps.services.Status.OK) {
+            console.log(result);
+            const { x, y } = result[0];
+
+            var options = { //지도를 생성할 때 필요한 기본 옵션
+                center: new kakao.maps.LatLng(y, x), //지도의 중심좌표.
+                level: 3 //지도의 레벨(확대, 축소 정도)
+            };
+            map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        }
+    });
 
     const div = document.createElement('div');
     div.innerHTML = `
