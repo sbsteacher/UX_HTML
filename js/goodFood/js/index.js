c9bb37f5751e11d1f1f38e0b2bd5146ebcba3ad5
ParkDoheum <pirbak@gmail.com>
2022-11-11 17:58:08 +0900

    paging(1)

--- js/goodFood/js/index.js
+++ js/goodFood/js/index.js
@@ -4,22 +4,54 @@
     const $select = document.querySelector('#gu');
     const $search = document.querySelector('#search');
     const $contentsContainer = document.querySelector('ul.contents_container');
+    const $paging = document.querySelector('.paging');
+
+    let data = null;
+    let page = 1;
+    //1 > 0~19
+    //2 > 20~39
+    //3 > 40~59
+    const rowCnt = 20; //페이지당 아이템 수
 
     $search.addEventListener('click', e => {                
-        $contentsContainer.innerHTML = null;
+        //$contentsContainer.innerHTML = null;
         const val = $select.value;        
         const url = `https://www.daegufood.go.kr/kor/api/tasty.html?mode=json&addr=${val}`;
         fetch(url)
         .then(res => res.json())
-        .then(makeList);
+        .then(makeDisplay);
     });
 
-    function makeList(res) {
-        console.log(res);
-        //res.data.forEach(makeItem);
-        res.data.forEach(function(item) {
+    function makeDisplay(res) {
+        data = res.data;
+        const itemLen = data.length;
+        console.log(itemLen);
+        const maxPage = Math.ceil(itemLen / rowCnt);
+        makePaging(maxPage);
+        makeList();
+    }
+
+    function makeList() {
+        $contentsContainer.innerHTML = null;
+        const sIdx = (page - 1) * rowCnt;
+        const eIdx = page * rowCnt;
+
+        for(let i=sIdx; i<eIdx; i++) {
+            const item = data[i];
             makeItem(item);
-        });
+        }
+    }
+
+    //페이징 숫자 찍기
+    function makePaging(maxPage) {
+        for(let i=1; i<=maxPage; i++) {
+            const span = document.createElement('span');
+            $paging.appendChild(span);
+            //span.className = 'page pointer';
+            span.classList.add('page');
+            span.classList.add('pointer');                        
+            span.textContent = i.toString();
+        }
     }
 
     function makeItem(item) {
