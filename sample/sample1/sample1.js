b76bff75470f2cb214d64465a1430c965c9f327a
ParkDoheum <pirbak@gmail.com>
2022-11-25 15:44:21 +0900

    sample1_탭화면

---
+++ sample/sample1/sample1.js
@@ -0,0 +1,16 @@
+(function() {
+    const $spanList = document.querySelectorAll('.sidebar span');
+    const $viewList = document.querySelectorAll('.p-relative > div');
+    
+    if($spanList) {
+        $spanList.forEach(item => {
+            item.addEventListener('click', e => {
+                $viewList.forEach(subItem => {
+                    subItem.classList.toggle('d-none', subItem.dataset.view !== e.target.dataset.view);
+                   
+                });
+            });
+        });
+    }
+        
+})();
