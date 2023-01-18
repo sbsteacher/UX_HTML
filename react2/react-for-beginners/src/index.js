3cbd5d23082514811a9bc6d90b3f4daddb3ed80c
ParkDoheum <pirbak@gmail.com>
2023-01-18 21:59:24 +0900

    보충 01-18

--- react2/react-for-beginners/src/index.js
+++ react2/react-for-beginners/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import App2 from './App2';
-import App3 from './App3';
-import App4 from './App4';
-import ToDoApp from './ToDoApp';
-import CoinTracker from './CoinTracker';
-import MovieApp from './MovieApp';
-//import './styles.css';
+import MovieApp from './MovieApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
